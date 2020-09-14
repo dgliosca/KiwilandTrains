70055c96f9ebe039f1bde97661f47498be96b876
diegogliosca <diego.gliosca@gmail.com>
2020-09-14 11:51:30 +0100

    added disabled acceptance tests

---
+++ src/test/kotlin/com/kiwilandtrainservice/acceptance/TrainRoutesTest.kt
@@ -0,0 +1,68 @@
+package com.kiwilandtrainservice.acceptance
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import kotlin.test.junit5.JUnit5Asserter.fail
+
+class TrainRoutesTest {
+
+    @Disabled
+    @Test
+    fun `The distance of the route A-B-C`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The distance of the route A-D`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The distance of the route A-D-C`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The distance of the route A-E-B-C-D`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The distance of the route A-E-D`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The number of trips starting at C and ending at C with a maximum of 3 stops`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The number of trips starting at A and ending at C with exactly 4 stops`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The length of the shortest route (in terms of distance to travel) from A to C`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The length of the shortest route (in terms of distance to travel) from B to B`() {
+        fail("Non implemented yet")
+    }
+
+    @Disabled
+    @Test
+    fun `The number of different routes from C to C with a distance of less than 30`() {
+        fail("Non implemented yet")
+    }
+}
