beac278c095fd0adf1b58476b678fc58728b108c
diegogliosca <diego.gliosca@gmail.com>
2020-09-14 12:02:15 +0100

    build a train service with two stations

--- src/test/kotlin/com/kiwilandtrainservice/TrainServiceTest.kt
+++ src/test/kotlin/com/kiwilandtrainservice/TrainServiceTest.kt
@@ -8,16 +8,33 @@ class TrainServiceTest {
 
     @Test
     fun `build an empty train service`() {
-        val trainService = TrainService()
+        val trainService = TrainService("")
 
         assertThat(trainService.stations(), equalTo(emptySet()))
     }
 
-    class TrainService {
-        fun stations(): List<Station> {
-            return emptyList()
+    @Test
+    fun `build a train service with two stations`() {
+        val trainService = TrainService("AB1")
+
+        assertThat(trainService.stations(), equalTo(setOf(Station("A"), Station("B"))))
+    }
+
+    class TrainService(route: String) {
+        private val stations: Set<Station>
+
+        init {
+            stations = if (route.isEmpty()) {
+                emptySet()
+            } else {
+                val source = route[0].toString()
+                val destination = route[1].toString()
+                setOf(Station(source), Station(destination))
+            }
         }
+
+        fun stations() = stations
     }
 }
 
-class Station
+data class Station(val name: String)
